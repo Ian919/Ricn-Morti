3cec0cb7f345a2d63cf407900098ed3ced7770d0
izhakovskii <sparta-sparta158@mail.ru>
2025-04-28 05:50:47 +0300

    feat(web):добавлена страница кароточки

--- app/api/hello/route.ts
+++
@@ -1,5 +0,0 @@
-// export async function GET() {
-//     return new Response(JSON.stringify({ message: "Hello from Rick & Morty!" }), {
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   }  
